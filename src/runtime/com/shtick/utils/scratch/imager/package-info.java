5ec2b078965df3ef23d73f262fdb4ab7706b08fe
Sean M. Cox <github@smcox.com>
2018-06-18 12:45:45 -0600

    Updated image viewer to allow opening any sb2/zip file, and addressed issue with font size being wrong when explicitly defined.

--- src/runtime/com/shtick/utils/scratch/imager/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Classes defining an UI for viewing project images.
- * This is intended to facilitate testing the rendering of scratch SVG files.
- * 
- * @author scox
- *
- */
-package com.shtick.utils.scratch.imager;
